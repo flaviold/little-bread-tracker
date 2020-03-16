971ce5c7a4a50fb3479eee64f33b1b4e934d859c
Waschington Luiz Menezes <wmenezes@elfsm.local>
2020-03-16 14:02:59 -0300

    home concluído - falta responsividade

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -7,27 +7,24 @@ import { Paozinho } from '../models/paozinho.component'
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  listaAniversarios: Array<any> = [{ tipo: 1, data: '20/08/2020'}, { tipo: 2, data: '12/05/2020' }, { tipo: 3,data: '10/06/2020' }, { tipo: 1, data: '10/07/2020' }];
+  listaAniversarios: Array<any> = [{ tipo: 1, nome: 'Waschington', data: '20/08/2020' }, { tipo: 2, nome: 'Flavio', data: '12/05/2020' }, { tipo: 3, data: '10/06/2020', nome: 'Ronny' }, { tipo: 1, nome: 'Chico', data: '10/07/2020' }, { tipo: 2, nome: 'Teste', data: '10/07/2020' }];
   proximoPao: any;
   constructor() { }
 
   ngOnInit() {
-    this.proximoPao = {
-      tipo: 1,
-      data: '18/08/2000',
-      nome: 'Waschington',
-    }
+   // this.contador();
   }
 
-  buscarTipo(){
-    if(this.proximoPao.tipo == 1) {
+  buscarTipo(item) {
+    if (item.tipo == 1) {
       return 'Aniversário';
-    }else if(this.proximoPao.tipo == 2){
+    } else if (item.tipo == 2) {
       return 'Férias';
-    }else {
+    } else {
       return 'Admissão'
     }
   }
+
   escolherIcon(item) {
     if (item.tipo == 1) { //Aniversário
       return 'fas fa-birthday-cake';
@@ -38,5 +35,20 @@ export class HomeComponent implements OnInit {
     }
   }
 
-}
+//   function days_between(date1, date2) {
 
+//   // The number of milliseconds in one day
+//   var ONE_DAY = 1000 * 60 * 60 * 24
+
+//   // Convert both dates to milliseconds
+//   var date1_ms = date1.getTime()
+//   var date2_ms = date2.getTime()
+
+//   // Calculate the difference in milliseconds
+//   var difference_ms = Math.abs(date1_ms - date2_ms)
+
+//   // Convert back to days and return
+//   return Math.round(difference_ms / ONE_DAY)
+
+// }
+}
